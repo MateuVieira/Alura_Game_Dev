0d2a09d6c065da90620081db73b113d52af9a0d8
Mateus Vieira <mateus17v@gmail.com>
2020-06-23 19:28:17 -0300

    Config project to use react-p5

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Sketch from 'react-p5';
 
-// import { Container } from './styles';
+import { Container } from './styles';
+
+const initialParams = {
+  position: {
+    X: 0,
+    Y: 0,
+  },
+  canvas: {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  },
+};
 
 function Main() {
+  const [posistion, setPosition] = useState({
+    x: initialParams.position.X,
+    y: initialParams.position.Y,
+  });
+
+  const setup = (p5, canvasParentRef) => {
+    const { width, height } = initialParams.canvas;
+
+    p5.createCanvas(width, height).parent(canvasParentRef);
+  };
+
+  const draw = (p5) => {
+    const { X, Y } = posistion;
+
+    p5.background(220);
+    // p5.ellipse(X, Y, 70, 70);
+    // setPosition({
+    //   ...posistion,
+    //   x: X + 1,
+    // });
+  };
+
   return (
-    <div>
-      <h1>Main Page</h1>
-    </div>
+    <Container>
+      <Sketch setup={setup} draw={draw} />
+    </Container>
   );
 }
 
